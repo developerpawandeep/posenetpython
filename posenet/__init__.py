cee6809823700e61f49675231db3760f1ff62956
developerpawandeep <cvdevs@datakalp.com>
2020-05-23 15:02:59 +0530

    updated code

---
+++ posenet/__init__.py
@@ -0,0 +1,4 @@
+from posenetpython.posenet.constants import *
+from posenetpython.posenet.decode_multi import decode_multiple_poses
+from posenetpython.posenet.model import load_model
+from posenetpython.posenet.utils import *
